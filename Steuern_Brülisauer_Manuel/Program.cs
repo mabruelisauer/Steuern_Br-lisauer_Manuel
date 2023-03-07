f3aecdaf39662c434957b4cbf1694b15d4abbd4a
mabruelisauer <112378881+mabruelisauer@users.noreply.github.com>
2023-03-07 07:04:50 +0100

    Some adjustments to the program

--- Steuern_Brülisauer_Manuel/Program.cs
+++ Steuern_Brülisauer_Manuel/Program.cs
@@ -10,18 +10,23 @@
 
             Person Joe_Miller = new Person(6400);
             Steuerzahlende[0] = Joe_Miller;
+            Joe_Miller.berechneSteuer();
 
             Arbeiter Jonny_Worker = new Arbeiter(36000);
             Steuerzahlende[1] = Jonny_Worker;
+            Jonny_Worker.berechneSteuer();
 
             Banker Fred_Moneymaker = new Banker(4000000);
             Steuerzahlende[2] = Fred_Moneymaker;
+            Fred_Moneymaker.berechneSteuer();
 
             Koenigin Silvia = new Koenigin(1000000);
             Steuerzahlende[3] = Silvia;
+            Silvia.berechneSteuer();
 
             Unternehmen MeierHansAG = new Unternehmen(500000);
             Steuerzahlende[4] = MeierHansAG;
+            MeierHansAG.berechneSteuer();
 
             Console.WriteLine(finanzamt.BerechneSteuer(Steuerzahlende));
         }
